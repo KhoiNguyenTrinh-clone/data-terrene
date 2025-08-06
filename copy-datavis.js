cbaab2861ee1673f12da566ea3317a3e4ca28682
Senryuu-0211 <nguyentk0211@gmail.com>
2025-08-06 23:37:07 +0700

    Fix Vercel build: CommonJS copy-datavis and output config

--- copy-datavis.js
+++ copy-datavis.js
@@ -1,5 +1,5 @@
 // copy-datavis.js
-import fs from 'fs-extra';
+const fs = require('fs-extra');
 
 fs.copySync('./public/datavis', './dist/datavis');
 console.log('✅ Đã copy datavis vào dist/datavis');
